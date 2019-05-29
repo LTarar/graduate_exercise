2cf0dfce0cbaf1cd233dee763a509c418e2f8158
Wardah <wlt_93@hotmail.com>
2019-05-29 21:47:04 +0100

    Display total number of todo items

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,8 @@
 const app = document.getElementById('root')
 const list = document.getElementById('list')
+const count = document.getElementsByClassName('count')[0];
+
+console.log("count: ", count)
 
 var request = new XMLHttpRequest()
 
@@ -8,10 +11,13 @@ request.onload = function() {
 
   var data = JSON.parse(this.response)
 
+  count.append(data.length);
+
   if (request.status >= 200 && request.status < 400) {
     data.slice().reverse().forEach(todo => {
       const li = document.createElement('li')
       li.setAttribute('class', 'todo')
+
       const title = todo.title
 
       li.append(title)
